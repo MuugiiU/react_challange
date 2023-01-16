f24014f8014a2fa03a56acb2773c33a08d4676d4
MuugiiU <classroom2leap@gmail.com>
2023-01-16 12:54:03 +0800

    finish

--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,8 @@
 export const players = [
-  { id: 1, name: "Bataa", score: 10 },
-  { id: 2, name: "Boldoo", score: 20 },
-  { id: 3, name: "Dorjoo", score: 30 },
-  { id: 4, name: "Tsetsgee", score: 10 },
-  { id: 5, name: "Dulmaa", score: 9 },
-  { id: 6, name: "Zorigoo", score: 15 },
+  { id: 1, name: "Батаа", score: 10 },
+  { id: 2, name: "Болдоо", score: 20 },
+  { id: 3, name: "Доржоо", score: 30 },
+  { id: 4, name: "Цэцгээ", score: 10 },
+  { id: 5, name: "Дулмаа", score: 9 },
+  { id: 6, name: "Зоригоо", score: 15 },
 ];
